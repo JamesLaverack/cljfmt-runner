c12cd0ee033a68046fa35423bbbcbd679fa2ec7b
James Laverack <james@jameslaverack.com>
2018-10-01 02:07:37 +0100

    Add example project to test with

--- test/cljfmt_runner/core_test.clj
+++ test/cljfmt_runner/core_test.clj
@@ -2,9 +2,12 @@
   (:require [clojure.test :refer :all]
             [cljfmt-runner.core :as rcore]))
 
+(def example-root "test-resources/example-project/")
+
 (deftest io-test
   (testing "Test that a directory can be detected"
-    (is (rcore/directory? (clojure.java.io/file "."))))
+    (is (rcore/directory? (clojure.java.io/file example-root))))
   (testing "Test that a file can be detected"
-    (is (rcore/file? (clojure.java.io/file "deps.edn")))))
-
+    (is (rcore/file? (clojure.java.io/file (str example-root "deps.edn")))))
+  (testing "Test that a Clojure source file can be detected"
+    (is (rcore/clojure-source? (clojure.java.io/file (str example-root "src/example/hello.clj"))))))
