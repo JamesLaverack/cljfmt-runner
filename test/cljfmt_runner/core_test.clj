055296e989a7b416e924743d41e7a0186a91ae31
James Laverack <james@jameslaverack.com>
2018-10-01 02:33:30 +0100

    Import io namesapce

--- test/cljfmt_runner/core_test.clj
+++ test/cljfmt_runner/core_test.clj
@@ -1,15 +1,16 @@
 (ns cljfmt-runner.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
             [cljfmt-runner.core :as rcore]))
 
 (def example-root "test-resources/example-project/")
 
 (deftest io-test
   (testing "Test that a directory can be detected"
-    (is (rcore/directory? (clojure.java.io/file example-root))))
+    (is (rcore/directory? (io/file example-root))))
   (testing "Test that a file can be detected"
-    (is (rcore/file? (clojure.java.io/file (str example-root "deps.edn")))))
+    (is (rcore/file? (io/file (str example-root "deps.edn")))))
   (testing "Test that a Clojure source file can be detected"
-    (is (rcore/clojure-source? (clojure.java.io/file (str example-root "src/example/hello.clj")))))
+    (is (rcore/clojure-source? (io/file (str example-root "src/example/hello.clj")))))
   (testing "Test that a ClojureScript source file can be detected"
-    (is (rcore/clojure-source? (clojure.java.io/file (str example-root "src/example/hello.cljs"))))))
+    (is (rcore/clojure-source? (io/file (str example-root "src/example/hello.cljs"))))))
