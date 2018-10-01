4a925ec68516a3981b45573ede60eb04a588ba1c
James Laverack <james@jameslaverack.com>
2018-10-01 02:00:10 +0100

    Add test for directory? function

--- test/cljfmt_runner/core_test.clj
+++ test/cljfmt_runner/core_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [cljfmt-runner.core :as rcore]))
 
-(deftest directory-test
+(deftest io-test
   (testing "Test that a directory can be detected"
-    (is (rcore/directory? (clojure.java.io/file ".")))))
+    (is (rcore/directory? (clojure.java.io/file "."))))
+  (testing "Test that a file can be detected"
+    (is (rcore/file? (clojure.java.io/file "deps.edn")))))
+
